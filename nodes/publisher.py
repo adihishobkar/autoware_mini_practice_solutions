e641d026b3ecc35718ee21b817d292813bfa6aa0
Linux User <linuxuser@linux-d2006-20.domenis.local>
2024-09-03 17:57:01 +0300

    Added parameter support to publisher node and updated launch file

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -4,6 +4,9 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
+
+message = rospy.get_param('~message', 'Hello World!')
+
 rate = rospy.Rate(2)
 pub = rospy.Publisher('/message', String, queue_size=10)
 
